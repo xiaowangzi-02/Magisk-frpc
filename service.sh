af42dd3fd555fc29458962e43ac3d6d391ce17ad
xiaowangzi-02 <77796989+xiaowangzi-02@users.noreply.github.com>
2024-02-04 05:21:18 +0000

    v0.2

--- service.sh
+++ service.sh
@@ -1,3 +1,7 @@
-#!/system/bin/sh
+MODDIR=${0%/*}
 
-$MODPATH/bin/frpc -c $MODPATH/etc/frpc.toml
+until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
+  sleep 1
+done
+
+nohup $MODDIR/bin/frpc -c $MODDIR/etc/frpc.toml > $MODDIR/log.txt 2>&1 &
