4ef8f847e7292e153bd5a19760acee03a502f034
xiaowangzi-02 <77796989+xiaowangzi-02@users.noreply.github.com>
2024-02-04 05:32:15 +0000

    fix: network error

--- service.sh
+++ service.sh
@@ -1,7 +1,16 @@
 MODDIR=${0%/*}
 
-until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
-  sleep 1
+# 检查网络连接的函数
+check_network() {
+    ping -c 1 dns.alidns.com > /dev/null 2>&1
+    return $?
+}
+
+# 等待网络连接
+while check_network
+do
+    echo "Waiting for network..."
+    sleep 5
 done
 
 nohup $MODDIR/bin/frpc -c $MODDIR/etc/frpc.toml > $MODDIR/log.txt 2>&1 &
